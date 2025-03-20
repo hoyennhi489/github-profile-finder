52f53ce8c389e57ec7f2db678544c2643d3ea692
hoyennhi489 <hyn489@gmail.com>
2025-03-20 20:21:59 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [
     react(),
-    ghPages()
   ],
-  base: '/github-profile-finder/',
+  base: '/github-profile-finder/', 
 });
